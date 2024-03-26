357a2db9624c63275e611ea24e85646326c043d0
dgrinche <daniel.grinchenko@gmail.com>
2024-03-26 14:38:09 -0400

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,10 +10,6 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-    module.hot.accept();
-}
-
 const controlRecipes = async function () {
     try {
         const id = window.location.hash.slice(1);
@@ -51,7 +47,6 @@ const controlSearchResults = async function () {
         await model.loadSeatchResults(query);
 
         // 3) Render results
-        // resultsView.render(model.state.search.results);
         resultsView.render(model.getSearchResultsPage());
 
         // 4) Render initial pagination buttons
@@ -74,7 +69,6 @@ const controlServings = function (newServings) {
     model.updateServings(newServings);
 
     // Update the recipe view
-    // recipeView.render(model.state.recipe);
     recipeView.update(model.state.recipe);
 };
 
@@ -126,10 +120,6 @@ const controlAddRecipe = async function (newRecipe) {
     }
 };
 
-const newFeature = new (function () {
-    console.log('Welcome to the application');
-})();
-
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -138,6 +128,5 @@ const init = function () {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHandlerUpload(controlAddRecipe);
-    newFeature();
 };
 init();
