340fc3f4e39305dd7a215116d8b3130078d45d85
dgrinche <daniel.grinchenko@gmail.com>
2024-03-26 13:46:52 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bb8281ca-4d05-4742-96b6-14e5dbd9d632';
+export const MODAL_CLOSE_SEC = 2.5;
